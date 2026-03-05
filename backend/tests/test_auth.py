8cd502c0c2edf71f86549b8dd6bea5872187e115
Helmi Touati <touatizh@gmail.com>
2026-03-05 04:01:04 +0100

    fix(test): resolve event loop conflict and admin user not found issues

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -1,13 +1,23 @@
 import pytest
 from app.core.config import settings
+from app.services.user_service import create_user
+from app.schemas.user import UserCreate
+from app.db.session import SessionLocal
 
 @pytest.mark.asyncio
 async def test_login_success(client):
+    async with SessionLocal() as session:
+        test_user: UserCreate = UserCreate(
+            username="test user",
+            password="testpassword123",
+        )
+        await create_user(session=session, user=test_user)
+
     response = await client.post(
         "/api/auth/login",
         json={
-            "username": settings.FIRST_SUPERUSER_USERNAME,
-            "password": settings.FIRST_SUPERUSER_PASSWORD.get_secret_value(),
+            "username": test_user.username,
+            "password": test_user.password,
         },
     )
     assert response.status_code == 200
