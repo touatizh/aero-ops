793593212987953243585fb4eeebcc0ee72d764c
Helmi Touati <touatizh@gmail.com>
2026-03-18 03:13:54 +0100

    Add flight statistics endpoint

--- backend/app/api/v1/admin.py
+++ backend/app/api/v1/admin.py
@@ -1,3 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from app.core.dependency import require_role
+from app.core.roles import OPS_OR_ADMIN
+from app.db.dependency import DbSession
+from app.models import User
+from app.schemas import FlightStatistics
+from app.services.flight_service import get_all_flights_statistics
 
 router: APIRouter = APIRouter(prefix="/admin", tags=["Admin"])
+
+
+@router.get("/statistics", response_model=FlightStatistics)
+async def get_flight_statistics(
+    session: DbSession, current_user: User = Depends(require_role(*OPS_OR_ADMIN))
+):
+    return await get_all_flights_statistics(session=session)
