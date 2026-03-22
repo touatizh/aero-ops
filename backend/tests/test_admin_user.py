f2eeb30ede3a8d9b45b98c8e5bfc5a2f3ad8aa91
Helmi Touati <touatizh@gmail.com>
2026-03-23 00:07:11 +0100

    Additional user management tests

--- backend/tests/test_admin_user.py
+++ backend/tests/test_admin_user.py
@@ -1,4 +1,3 @@
-from pydantic_core import core_schema
 import pytest
 
 from app.schemas import UserRead, UserReadWithStats
@@ -153,3 +152,111 @@ async def test_ops_get_user_with_zero_details(client, test_ops, auth_headers):
     assert result.get("total_flights") == 0
     assert result.get("total_flight_hours") == 0
     assert result.get("pending_flights") == 0
+
+
+@pytest.mark.asyncio
+async def test_ops_create_user_with_role(client, test_admin, auth_headers):
+    headers = await auth_headers(test_admin)
+
+    result = await client.post(
+        "/api/admin/users",
+        json={
+            "username": "new_user_with_role",
+            "password": "password123",
+            "role": Role.OPS
+        },
+        headers=headers
+    )
+
+    assert result.status_code == 200
+
+
+@pytest.mark.asyncio
+async def test_duplicate_username(client, test_admin, auth_headers):
+    headers = await auth_headers(test_admin)
+
+    result = await client.post(
+        "/api/admin/users",
+        json={
+            "username": "test_admin",
+            "password": "password123",
+            "role": Role.ADMIN
+        },
+        headers=headers
+    )
+
+    assert result.status_code == 400
+
+
+
+@pytest.mark.asyncio
+async def test_unvalid_user_create_data(client, test_admin, auth_headers):
+    headers = await auth_headers(test_admin)
+
+    result = await client.post(
+        "/api/admin/users",
+        json={
+            "username": "ww",
+            "password": "password123",
+            "role": Role.PI
+        },
+        headers=headers
+    )
+
+    assert result.status_code == 422
+
+    result = await client.post(
+        "/api/admin/users",
+        json={
+            "username": "www",
+            "password": "pwd",
+            "role": Role.PI
+        },
+        headers=headers
+    )
+
+    assert result.status_code == 422
+
+
+@pytest.mark.asyncio
+async def test_admin_update_user(client, test_admin, test_pilot, auth_headers):
+    headers = await auth_headers(test_admin)
+
+    result = await client.patch(
+        f"/api/admin/users/{test_pilot.id}",
+        json={
+            "username": "pilot_promoted_ops",
+            "password": "newpassword123",
+            "role": Role.OPS,
+            "is_active": False,
+        },
+        headers=headers
+    )
+
+    assert result.status_code == 200
+
+    result = result.json()
+
+    assert result.get("username") == "pilot_promoted_ops"
+    assert result.get("role") == Role.OPS
+    assert result.get("is_active") == False
+
+    attempt_login = await client.post("/api/auth/login", json={"username": "pilot_promoted_ops", "password": "newpassword123"})
+    assert attempt_login.status_code == 200
+
+
+
+
+@pytest.mark.asyncio
+async def test_admin_updates_non_existent_user(client, test_admin, auth_headers):
+    headers = await auth_headers(test_admin)
+
+    result = await client.patch(
+        "/api/admin/users/2bf92148-d56e-4e2e-b5b0-1e5d6745cf78",
+        json={
+            "username": "doesnt_exist",
+        },
+        headers=headers
+    )
+
+    assert result.status_code == 404
